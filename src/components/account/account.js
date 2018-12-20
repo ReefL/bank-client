fbbdc22781ca109132cf8be8193fd19d42e286ef
galamo <galamouyal88@gmail.com>
2018-12-20 21:36:56 +0200

    client app

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -3,9 +3,31 @@ import React, { Component } from "react";
 export default class Account extends Component {
   constructor(props) {
     super(props);
+
+    this.state = { accounts: [] };
+  }
+
+  componentDidMount() {
+    fetch("http://10.103.50.39:2200/accounts")
+      .then(result => {
+        return result.json();
+      })
+      .then(result => {
+        this.setState({
+          accounts: result
+        });
+      });
   }
 
   render() {
-    return <h1> Account Page </h1>;
+    return (
+      <div>
+        <h2> This is the account page </h2>
+
+        {this.state.accounts.map(curAccount => {
+          return <h2> {curAccount.account_number} </h2>;
+        })}
+      </div>
+    );
   }
 }
